adff58fce1eff76a3581e39ca03d2100604ce01c
Yuriy Grigorev <niceninjabox@gmail.com>
2022-02-14 21:45:57 +1000

    pre calc

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+ let op;
+function func() {
+    
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ function calc() {
+     let expression = document.getElementById("expression").value;
+     let array = expression.split('');
+     let num1 = Number(array[0]);
+     let num2 = Number(array[2]);
+     
+     let result;
+
+        switch (array[1]) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+        }
+
+      document.getElementById('result').innerHTML=result;
+ };
