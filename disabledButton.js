bbc24303841451f430fbf6c8057bdb8a9807496a
Yuriy Grigorev <niceninjabox@gmail.com>
2022-02-15 17:52:17 +1000

    disable button function

--- disabledButton.js
+++ disabledButton.js
@@ -1,1 +1,13 @@
 
+
+function disabledButton() {
+    let expression = document.getElementById("exp").value;
+    let button = document.getElementById("calc");
+    
+    if (expression) {
+        button.removeAttribute("disabled"); 
+    }
+    else {
+        button.setAttribute('disabled', 'disabled')
+    }
+};
